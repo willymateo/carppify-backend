a47d4b4a8fdf4d35e1c12c0394688baa3f71200e
willymateo <wjmateo@espol.edu.ec>
2022-09-05 22:31:39 -0500

    add db models

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,6 +3,11 @@ import { env } from "../config/app.config.js";
 import { Sequelize } from "sequelize";
 
 const config = dbConfig[env];
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
 export { sequelize };
---
+++ src/db/models/vehicle.js
@@ -0,0 +1,75 @@
+import { sequelize } from "../connection";
+import { DataTypes } from "sequelize";
+
+const Vehicle = sequelize.define(
+  "vehicle",
+  {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    driver_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "driver",
+        key: "id",
+      },
+    },
+    plate: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    model: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      comment: "bicycle | motorcycle | car | van | truck",
+    },
+    capacity: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      comment: "small | medium | large",
+    },
+    creation_date: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    tableName: "vehicle",
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [{ name: "id" }],
+      },
+      {
+        name: "type_idx",
+        using: "BTREE",
+        fields: [{ name: "type" }],
+      },
+      {
+        name: "creation_date_idx",
+        using: "BTREE",
+        fields: [{ name: "creation_date" }],
+      },
+      {
+        name: "driver_key",
+        using: "BTREE",
+        fields: [{ name: "driver_id" }],
+      },
+    ],
+  }
+);
+
+export { Vehicle };
