f0db70280b12aabe4f93078243875c72d42f5b18
willymateo <wjmateo@espol.edu.ec>
2022-09-07 10:42:00 -0500

    create route get driver by id

--- src/controllers/driver.controller.js
+++ src/controllers/driver.controller.js
@@ -23,4 +23,29 @@ const getVehicles = async (req, res) => {
   }
 };
 
-export { getVehicles };
+const getDriverById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const driver = await Driver.findByPk(id);
+
+    if (!driver) {
+      return res.status(404).send({ error: "Driver not found" });
+    }
+
+    const company = await driver.getCompany();
+
+    return res.status(200).send({
+      ...driver.dataValues,
+      company_id: undefined,
+      company,
+    });
+  } catch (error) {
+    console.log(
+      "Some error ocurred in getDriverById method of driver controller",
+      error
+    );
+    return res.status(409).send({ error: "Some error ocurred" });
+  }
+};
+
+export { getVehicles, getDriverById };
--- src/routes/driver.routes.js
+++ src/routes/driver.routes.js
@@ -1,7 +1,13 @@
-import { getVehicles } from "../controllers/driver.controller.js";
 import { Router } from "express";
+import {
+  getVehicles,
+  getDriverById,
+} from "../controllers/driver.controller.js";
 
 const router = Router();
+
+// Get driver by id
+router.get("/:id", getDriverById);
 
 // Get all vehicles by driver id
 router.get("/:id/vehicles", getVehicles);
